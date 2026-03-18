53a8b35ece737a7846bae12b48253b02cfaebe3f
akt33v <hengtianli555@gmail.com>
2026-03-18 22:19:42 +0800

    update model

--- src/model/Venue.java
+++ src/model/Venue.java
@@ -6,29 +6,74 @@ package model;
 
 /**
  *
- * @author KTYJ
+ * @author User
  */
 public class Venue {
-
     private String venueId;
     private String venueName;
-    private boolean blocked;
-
-    public Venue(String venueId, String venueName) {
+    private VenueStatus status;
+    private int capacity;
+    
+    public Venue(){
+    }
+    
+    public Venue(String venueId, String venueName, VenueStatus status, int capacity) {
         this.venueId = venueId;
         this.venueName = venueName;
-        this.blocked = false;
+        this.status = status;
+        this.capacity = capacity;
     }
-
-    public void blockVenue() {
-        blocked = true;
+    
+    public Venue(String venueId, String venueName, int capacity){
+        this.venueId = venueId;
+        this.venueName = venueName;
+        this.status = VenueStatus.AVAILABLE; //default available
+        this.capacity = capacity;
     }
-
-    public void unblockVenue() {
-        blocked = false;
+    
+    public String getId() {
+        return venueId;
     }
-
-    public boolean isBlocked() {
-        return blocked;
+    
+    public String getVenueName(){
+        return venueName;
+    }
+    
+    public VenueStatus getStatus() {
+        return status;
+    }
+    
+    public int getCapacity(){
+        return capacity;
+    }
+    
+    public void setVenueId(String venueId){
+        this.venueId = venueId;
+    }
+    
+    public void setVenueName(String venueName){
+        this.venueName = venueName;
+    }
+    
+    public void setStatus(VenueStatus status){
+        this.status = status;
+    }
+    
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    
+    public boolean isAvailable(){
+        return status == VenueStatus.AVAILABLE;
+    }
+    
+    @Override
+    public String toString(){
+        return "Venue{" +
+                "id='" + venueId + '\'' +
+                ", venueName='" + venueName + '\'' +
+                ", status=" + status +
+                ", capacity=" + capacity +
+                '}';
     }
 }
